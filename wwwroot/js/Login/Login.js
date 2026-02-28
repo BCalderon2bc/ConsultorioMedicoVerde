70933614ffeb0e61480758c5fd53ef508987e974
Andy <Usuario@LAPTOP-5PS7B1PH>
2026-02-28 12:06:08 -0600

    Refactor login: separación de JS, centralización de estilos en site.css y mejora UI

---
+++ wwwroot/js/Login/Login.js
@@ -0,0 +1,41 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    
+    /* ===============================
+       TOGGLE PASSWORD
+    =============================== */
+    const toggleBtn = document.querySelector(".toggle-password");
+    const passwordField = document.getElementById("password");
+    const icon = document.getElementById("toggleIcon");
+
+    if (toggleBtn && passwordField && icon) {
+        toggleBtn.addEventListener("click", function () {
+
+            if (passwordField.type === "password") {
+                passwordField.type = "text";
+                icon.classList.remove("bi-eye");
+                icon.classList.add("bi-eye-slash");
+            } else {
+                passwordField.type = "password";
+                icon.classList.remove("bi-eye-slash");
+                icon.classList.add("bi-eye");
+            }
+
+        });
+    }
+
+
+    /* ===============================
+       BOTÓN LOADING LOGIN
+    =============================== */
+    const form = document.querySelector("form");
+    const btn = document.getElementById("btnLogin");
+    const text = document.getElementById("btnText");
+
+    if (form && btn && text) {
+        form.addEventListener("submit", function () {
+            btn.disabled = true;
+            text.innerHTML = "Ingresando...";
+        });
+    }
+
+});
